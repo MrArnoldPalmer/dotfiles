4a2f6b735c73410ca6d34dfe69f166f175575d8d
Mitchell Valine <mitchellvaline@yahoo.com>
2021-01-22 10:29:49 -0800

    Add setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm ~/.config/alacritty/alacritty.yml
+cp ./alacritty.yml ~/.config/alacritty/alacritty.yml
+
+rm ~/.config/nvim/init.vim
+cp ./neovim.vim ~/.config/nvim/init.vim
+
+rm ~/.tmux.conf
+cp ./tmux.conf ~/.tmux.conf
+
+rm ~/.zshrc
+cp ./zshrc ~/.zshrc
