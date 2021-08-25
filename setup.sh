dc72ccbfa060ad48c99852a5fbb1db72a79e12d4
Mitchell Valine <mitchellvaline@yahoo.com>
2021-08-25 12:36:25 -0700

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,38 +1,46 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 ## Alacritty
-if ![[-d "~/.config/alacritty"]]; then
+if [[ ! -d ~/.config/alacritty ]]; then
+  echo "alacritty dir doesn't exist, make it!"
   mkdir ~/.config/alacritty
 fi
 
-if [[-f "~/.config/alacritty/alacritty.yml"]]; then
+if [[ -e ~/.config/alacritty/alacritty.yml ]]; then
+  echo "alacritty config exists, remove it!"
   rm ~/.config/alacritty/alacritty.yml
 fi
-ln -sv ./alacritty.yml ~/.config/alacritty/alacritty.yml
+ln -sv $SCRIPT_DIR/alacritty.yml ~/.config/alacritty/alacritty.yml
 
 ## Neovim
-if ![[-d "~/.config/nvim"]]; then
+if [[ ! -d ~/.config/nvim ]]; then
+  echo "nvim dir doesn't exist, make it!"
   mkdir ~/.config/nvim
 fi
 
-if [[-f "~/.config/nvim/init.vim"]]; then
+if [[ -e ~/.config/nvim/init.vim ]]; then
+  echo "nvim config exists, remove it!"
   rm ~/.config/nvim/init.vim
 fi
-ln -sv ./neovim.vim ~/.config/nvim/init.vim
+ln -sv $SCRIPT_DIR/neovim.vim ~/.config/nvim/init.vim
 
 ## Tmux
-if [[-f "~/.tmux.conf"]]; then
+if [[ -e ~/.tmux.conf ]]; then
+  echo "tmux config exists, remove it!"
   rm ~/.tmux.conf
 fi
-ln -sv ./tmux.conf ~/.tmux.conf
+ln -sv $SCRIPT_DIR/tmux.conf ~/.tmux.conf
 
 ## ZSH
-if [[-f "~/.zshrc"]]; then
+if [[ -e ~/.zshrc ]]; then
+  echo "zsh config exists, remove it!"
   rm ~/.zshrc
 fi
-ln -sv ./zshrc ~/.zshrc
+ln -sv $SCRIPT_DIR/zshrc ~/.zshrc
 
-if [[-f "~/.p10k.zsh"]]; then
+if [[ -e ~/.p10k.zsh ]]; then
+  echo "p10k config exists, remove it!"
   rm ~/.p10k.zsh
 fi
-ln -sv ./p10k.zsh ~/.p10k.zsh
+ln -sv $SCRIPT_DIR/p10k.zsh ~/.p10k.zsh
