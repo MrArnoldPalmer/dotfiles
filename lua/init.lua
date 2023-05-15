778194488e3f62431194bcc9ff0787f0c3815fe2
Mitchell Valine <mitchellvaline@yahoo.com>
2023-05-15 15:57:50 -0700

    make lua config better

---
+++ lua/init.lua
@@ -0,0 +1,19 @@
+local is_wsl = (function() 
+  local output = vim.fn.systemlist "uname -r" 
+  return not not string.find(output[1] or "", "WSL")
+end)() 
+
+if(is_wsl)
+then
+  vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+      ["+"] = "clip.exe",
+      ["*"] = "clip.exe",
+    },
+    paste = {
+      ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+      ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    },
+  }
+end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,20 +1,44 @@
 local plugins = {
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "rust-analyzer",
-        "typescript-language-server",
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = {
+				"vim",
+				"lua",
+				"rust",
+				"typescript",
+				"javascript",
+				"markdown",
+				"markdown_inline",
+				"json",
+				"go",
+				"java",
+			},
+		},
+	},
+	{
+		"williamboman/mason.nvim",
+		opts = {
+			ensure_installed = {
+				"rust-analyzer",
+				"stylua",
+				"typescript-language-server",
+			},
+		},
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			"jose-elias-alvarez/null-ls.nvim",
+			config = function()
+				require("custom.configs.null-ls")
+			end,
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end,
+	},
 }
 
 return plugins
