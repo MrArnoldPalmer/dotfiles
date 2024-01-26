eba7f42937c2ed6b1f070218275f2e50dbac2861
Mitchell Valine <mitchellvaline@yahoo.com>
2024-01-26 12:10:56 -0800

    remove rust-tools for rustaceanvim

--- lua/configs/rust-tools.lua
+++ lua/configs/rust-tools.lua
@@ -1,25 +1,23 @@
-local rt = require("rust-tools")
-local utils = require("core.utils")
-local configs = require("plugins.configs.lspconfig")
-local on_attach = configs.on_attach
-local capabilities = configs.capabilities
+vim.g.rustaceanvim = function()
+  local rt = require("rustaceanvim")
+  local utils = require("core.utils")
+  local configs = require("plugins.configs.lspconfig")
+  local on_attach = configs.on_attach
+  local capabilities = configs.capabilities
 
-local mason_path = vim.fn.stdpath("data") .. "/mason"
-local extension_path = mason_path .. "/packages/codelldb/extension"
-local codelldb_path = mason_path .. "/bin/codelldb"
-local liblldb_path = extension_path .. "/lldb/lib/liblldb.dylib"
-
-local opts = {
-	server = {
-		on_attach = function(client, bufnr)
-			on_attach(client, bufnr)
-		end,
-		capabilities = capabilities,
-	},
-	dap = {
-		adapter = require("rust-tools.dap").get_codelldb_adapter(codelldb_path, liblldb_path),
-	},
-}
-
--- Normal setup
-require("rust-tools").setup(opts)
+  local mason_path = vim.fn.stdpath("data") .. "/mason"
+  local extension_path = mason_path .. "/packages/codelldb/extension"
+  local codelldb_path = mason_path .. "/bin/codelldb"
+  local liblldb_path = extension_path .. "/lldb/lib/liblldb.dylib"
+  return {
+    server = {
+      on_attach = function(client, bufnr)
+        on_attach(client, bufnr)
+      end,
+      capabilities = capabilities,
+    },
+    dap = {
+      adapter = require("rustaceanvim.config").get_codelldb_adapter(codelldb_path, liblldb_path),
+    },
+  }
+end
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,200 +1,204 @@
 local plugins = {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"vim",
-				"lua",
-				"rust",
-				"toml",
-				"typescript",
-				"javascript",
-				"markdown",
-				"markdown_inline",
-				"json",
-				"go",
-				"gomod",
-				"java",
-				"dockerfile",
-				"python",
-				"dap_repl",
-			},
-		},
-		dependencies = {
-			{
-				"LiadOz/nvim-dap-repl-highlights",
-			},
-		},
-	},
-	{
-		"williamboman/mason.nvim",
-		opts = {
-			ensure_installed = {
-				"rust-analyzer",
-				"stylua",
-				"typescript-language-server",
-				"js-debug-adapter",
-				"codelldb",
-				"lua-language-server",
-			},
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		dependencies = {
-			{
-				"jose-elias-alvarez/null-ls.nvim",
-				config = function()
-					require("custom.configs.null-ls")
-				end,
-			},
-		},
-		config = function()
-			require("plugins.configs.lspconfig")
-		end,
-	},
-	{
-		"simrat39/rust-tools.nvim",
-		config = function()
-			require("custom.configs.rust-tools")
-		end,
-		dependencies = { "neovim/nvim-lspconfig", "mfussenegger/nvim-dap", "nvim-lua/plenary.nvim" },
-		ft = { "rust" },
-	},
-	{
-		"pmizio/typescript-tools.nvim",
-		dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-		config = function()
-			require("custom.configs.typescript-tools")
-		end,
-		ft = { "typescript", "javascript" },
-		opts = {},
-	},
-	{
-		"mfussenegger/nvim-dap",
-		config = function()
-			require("custom.configs.nvim-dap")
-		end,
-		dependencies = { "nvim-telescope/telescope-dap.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-dap.nvim",
-		dependencies = { "mfussenegger/nvim-dap", "nvim-telescope/telescope.nvim" },
-	},
-	{
-		"theHamsta/nvim-dap-virtual-text",
-		opts = {},
-		dependencies = { "mfussenegger/nvim-dap" },
-	},
-	{
-		"LiadOz/nvim-dap-repl-highlights",
-		config = function()
-			require("nvim-dap-repl-highlights").setup()
-		end,
-		dependencies = { "mfussenegger/nvim-dap" },
-	},
-	{
-		"rcarriga/cmp-dap",
-		config = function()
-			require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
-				sources = {
-					{ name = "dap" },
-				},
-			})
-		end,
-		dependencies = { "mfussenegger/nvim-dap" },
-	},
-	{
-		"ellisonleao/glow.nvim",
-		config = true,
-		cmd = "Glow",
-	},
-	{
-		"folke/trouble.nvim",
-		lazy = false,
-	},
-	{
-		"glepnir/lspsaga.nvim",
-		event = "LspAttach",
-		config = function()
-			require("custom.configs.lspsaga")
-		end,
-		dependencies = { "neovim/nvim-lspconfig" },
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "*",
-		event = "VeryLazy",
-		opts = {},
-	},
-	{
-		"nvim-telescope/telescope.nvim",
-		opts = {
-			extensions_list = { "themes", "terms", "fzf", "neoclip", "live_grep_args", "file_browser", "dap" },
-		},
-		dependencies = {
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-			},
-		},
-	},
-	{
-		"AckslD/nvim-neoclip.lua",
-		config = function()
-			require("neoclip").setup()
-		end,
-		dependencies = { "nvim-telescope/telescope.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-live-grep-args.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim" },
-	},
-	{
-		"nvim-telescope/telescope-file-browser.nvim",
-		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-	},
-	{
-		"pwntester/octo.nvim",
-		cmd = "Octo",
-		config = function()
-			require("octo").setup()
-		end,
-	},
-	{
-		"sindrets/diffview.nvim",
-		keys = {
-			{
-				"<leader>gvh",
-				":DiffviewFileHistory %<CR>",
-				desc = "Diffview history of the current file",
-			},
-			{
-				"<leader>gvo",
-				":DiffviewOpen -- %<CR>",
-				desc = "Open diffview for the current file",
-			},
-			{
-				"<leader>gvm",
-				":DiffviewOpen upstream/main %<CR>",
-				desc = "Open diffview  against upstream/main",
-			},
-			{
-				"<leader>gvc",
-				":DiffviewClose<CR>",
-				desc = "Close diffview",
-			},
-		},
-	},
-	{
-		"ruifm/gitlinker.nvim",
-		config = function()
-			require("gitlinker").setup()
-		end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "rust",
+        "toml",
+        "typescript",
+        "javascript",
+        "markdown",
+        "markdown_inline",
+        "json",
+        "go",
+        "gomod",
+        "java",
+        "dockerfile",
+        "python",
+        "dap_repl",
+      },
+    },
+    dependencies = {
+      {
+        "LiadOz/nvim-dap-repl-highlights",
+      },
+    },
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "rust-analyzer",
+        "stylua",
+        "typescript-language-server",
+        "js-debug-adapter",
+        "codelldb",
+        "lua-language-server",
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+          require("custom.configs.null-ls")
+        end,
+      },
+    },
+    config = function()
+      require("plugins.configs.lspconfig")
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    config = function()
+      require("custom.configs.rust-tools")
+    end,
+    version = "^4",
+    ft = { "rust" },
+    dependencies = {
+      "neovim/nvim-lspconfig",
+    },
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    config = function()
+      require("custom.configs.typescript-tools")
+    end,
+    ft = { "typescript", "javascript" },
+    opts = {},
+  },
+  {
+    "mfussenegger/nvim-dap",
+    config = function()
+      require("custom.configs.nvim-dap")
+    end,
+    dependencies = { "nvim-telescope/telescope-dap.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-dap.nvim",
+    dependencies = { "mfussenegger/nvim-dap", "nvim-telescope/telescope.nvim" },
+  },
+  {
+    "theHamsta/nvim-dap-virtual-text",
+    opts = {},
+    dependencies = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "LiadOz/nvim-dap-repl-highlights",
+    config = function()
+      require("nvim-dap-repl-highlights").setup()
+    end,
+    dependencies = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "rcarriga/cmp-dap",
+    config = function()
+      require("cmp").setup.filetype({ "dap-repl", "dapui_watches", "dapui_hover" }, {
+        sources = {
+          { name = "dap" },
+        },
+      })
+    end,
+    dependencies = { "mfussenegger/nvim-dap" },
+  },
+  {
+    "ellisonleao/glow.nvim",
+    config = true,
+    cmd = "Glow",
+  },
+  {
+    "folke/trouble.nvim",
+    lazy = false,
+  },
+  {
+    "glepnir/lspsaga.nvim",
+    event = "LspAttach",
+    config = function()
+      require("custom.configs.lspsaga")
+    end,
+    dependencies = { "neovim/nvim-lspconfig" },
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    opts = {
+      extensions_list = { "themes", "terms", "fzf", "neoclip", "live_grep_args", "file_browser", "dap" },
+    },
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build =
+        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+      },
+    },
+  },
+  {
+    "AckslD/nvim-neoclip.lua",
+    config = function()
+      require("neoclip").setup()
+    end,
+    dependencies = { "nvim-telescope/telescope.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-live-grep-args.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+  },
+  {
+    "pwntester/octo.nvim",
+    cmd = "Octo",
+    config = function()
+      require("octo").setup()
+    end,
+  },
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      {
+        "<leader>gvh",
+        ":DiffviewFileHistory %<CR>",
+        desc = "Diffview history of the current file",
+      },
+      {
+        "<leader>gvo",
+        ":DiffviewOpen -- %<CR>",
+        desc = "Open diffview for the current file",
+      },
+      {
+        "<leader>gvm",
+        ":DiffviewOpen upstream/main %<CR>",
+        desc = "Open diffview  against upstream/main",
+      },
+      {
+        "<leader>gvc",
+        ":DiffviewClose<CR>",
+        desc = "Close diffview",
+      },
+    },
+  },
+  {
+    "ruifm/gitlinker.nvim",
+    config = function()
+      require("gitlinker").setup()
+    end,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
 
 return plugins
