aa80b29de7612b5d8b4e765c1ba71565b4b3943e
Mitchell Valine <valinm@amazon.com>
2025-11-14 19:59:28 +0000

    fix snippets cmp

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -348,18 +348,28 @@ local plugins = {
 			end
 		end,
 	},
+  {
+  	"L3MON4D3/LuaSnip",
+  	-- follow latest release.
+  	version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  	-- install jsregexp (optional!).
+  	build = "make install_jsregexp",
+    dependencies = { "rafamadriz/friendly-snippets" },
+  },
 	{
 		"hrsh7th/nvim-cmp",
 		version = false, -- last release is way too old
 		event = "InsertEnter",
 		dependencies = {
+      "L3MON4D3/LuaSnip",
 			"hrsh7th/cmp-nvim-lsp",
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-cmdline",
-			"saadparwaiz1/cmp_luasnip",
+      "saadparwaiz1/cmp_luasnip",
 		},
 		config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
 			require("plugins.configs.cmp")
 		end,
 	},
